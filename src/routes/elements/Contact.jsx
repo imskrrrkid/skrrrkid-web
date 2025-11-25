3546ceaea51b1242c32c2702c6448766a09cd499
imskrrrkid <imskrrrkid@gmail.com>
2025-11-26 00:50:54 +0500

    Add: .env integration

--- src/routes/elements/Contact.jsx
+++ src/routes/elements/Contact.jsx
@@ -3,46 +3,50 @@ import { useState } from "react";
 
 function Contact() {
   const [result, setResult] = useState("");
+  const apiKey = import.meta.env.VITE_CONTACTER;
 
   const onSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
-    formData.append("access_key", "XYZ");
 
-    const response = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      body: formData,
-    });
+    formData.append("access_key", apiKey);
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData,
+      });
 
-    const data = await response.json();
-    setResult(data.success ? "Success!" : "Error");
+      const data = await response.json();
+      setResult(data.success ? "Success!" : "Error submitting form.");
+    } catch (error) {
+      console.error(error);
+      setResult("Error submitting form.");
+    }
   };
+
   return (
-    <>
-      <div className={ContactCss.base}>
-        <div className={ContactCss.haq}>
-          <h1>Have A Question?</h1>
-          <p>Go on, Don't hesitate. I would love to hear you!</p>
-          <div className={ContactCss.contactdetails}>
-            <p>
-              <a href="mailto:skrrrextra@gmail.com">
-                Mail: skrrrextra@gmail.com
-              </a>
-            </p>
-          </div>
-        </div>
-        <div className={ContactCss.form}>
-          <form onSubmit={onSubmit}>
-            <input type="text" name="name" required placeholder="Name" />
-            <input type="email" name="email" required placeholder="email" />
-            <textarea name="message" required></textarea>
-            <button type="submit">Submit</button>
-            <div class="h-captcha" data-captcha="true"></div>
-            <p>{result}</p>
-          </form>
+    <div className={ContactCss.base}>
+      <div className={ContactCss.haq}>
+        <h1>Have A Question?</h1>
+        <p>Go on, Don't hesitate. I would love to hear you!</p>
+        <div className={ContactCss.contactdetails}>
+          <p>
+            <a href="mailto:skrrrextra@gmail.com">Mail: skrrrextra@gmail.com</a>
+          </p>
         </div>
       </div>
-    </>
+      <div className={ContactCss.form}>
+        <form onSubmit={onSubmit}>
+          <input type="text" name="name" required placeholder="Name" />
+          <input type="text" name="subject" required placeholder="Subject" />
+          <input type="email" name="email" required placeholder="Email" />
+          <textarea name="message" required placeholder="Message"></textarea>
+          <button type="submit">Submit</button>
+          <div className="h-captcha" data-captcha="true"></div>
+          <p>{result}</p>
+        </form>
+      </div>
+    </div>
   );
 }
 
