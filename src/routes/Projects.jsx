43fc7f2cc63b5326bf73bcc535ab472d45c51577
imskrrrkid <imskrrrkid@gmail.com>
2025-12-08 23:58:39 +0500

    Check: Checking fuction on mobile

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,18 +1,48 @@
 import Projecter from "./Project.module.css";
+// import ProjectThumb from "./elements/ProjectThumb";
+// function Project() {
+//   return (
+//     <>
+//       <div className={Projecter.home}>
+//         <div className={Projecter.topTitle}>
+//           <h1>Projects</h1>
+//         </div>
+//         <div className={Projecter.catalog}>
+//           <ProjectThumb />
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Project;
+
+import { useState } from "react";
 import ProjectThumb from "./elements/ProjectThumb";
-function Project() {
+import ProjectCards from "./elements/ProjectCards";
+import Project3D from "./elements/Project3D";
+
+function Projects() {
+  const [screen, setScreen] = useState("thumb");
+
   return (
     <>
       <div className={Projecter.home}>
-        <div className={Projecter.topTitle}>
-          <h1>Projects</h1>
-        </div>
-        <div className={Projecter.catalog}>
-          <ProjectThumb />
-        </div>
+        {screen === "thumb" && (
+          <ProjectThumb
+            openGames={() => setScreen("games")}
+            open3D={() => setScreen("3d")}
+          />
+        )}
+
+        {screen === "games" && (
+          <ProjectCards closeCont={() => setScreen("thumb")} />
+        )}
+
+        {screen === "3d" && <Project3D closeCont={() => setScreen("thumb")} />}
       </div>
     </>
   );
 }
 
-export default Project;
+export default Projects;
--- src/routes/elements/ProjectCards.jsx
+++ src/routes/elements/ProjectCards.jsx
@@ -1,37 +1,54 @@
 import projectcardcss from "../elements/ProjectCars.module.css";
 import Project from "../Projects";
 import Card from "./cards/Card";
-import { useState } from "react";
-function ProjectCards() {
-  const [back, setBack] = useState("projectcards");
-  if (back === "project") {
-    return <Project closeCont={() => setBack("projectcards")} />;
-  }
-  const proLinked = () => {
-    return "#";
-  };
-  return (
-    <>
-      <div className={projectcardcss.mainCont}>
-        {/* Do no Delete this comment*/}
-        {/* <div
-          className={projectcardcss.close}
-          onClick={() => setBack("project")}
-        >
-          <button>Back</button>
-        </div>*/}
+// import { useState } from "react";
+// function ProjectCards() {
+//   const [back, setBack] = useState("projectcards");
+//   if (back === "project") {
+//     return <Project closeCont={() => setBack("projectcards")} />;
+//   }
+//   // const proLinked = () => {
+//   //   return "#";
+//   // };
+//   return (
+//     <>
+//       <div className={projectcardcss.mainCont}>
+//         <div
+//           className={projectcardcss.close}
+//           onClick={() => setBack("project")}
+//         >
+//           <button>Back</button>
+//         </div>
 
-        <div className={projectcardcss.cardslider}>
-          {/* // TODO: Add Dynamic Title and names + img which will link to data base */}
-          <Card
-            titleName="DPT"
-            paraInfo="DPT is an horror first person game about corrupt ceo of small private company (DPT)."
-            isDisabled={true}
-            proLink={proLinked()}
-          />
-        </div>
+//         <div className={projectcardcss.cardslider}>
+//           {/* // TODO: Add Dynamic Title and names + img which will link to data base */}
+//           <Card
+//             titleName="DPT"
+//             paraInfo="DPT is an horror first person game about corrupt ceo of small private company (DPT)."
+//             isDisabled={true}
+//           />
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+function ProjectCards({ closeCont }) {
+  return (
+    <div className={projectcardcss.mainCont}>
+      <div className={projectcardcss.close} onClick={closeCont}>
+        <button>Back</button>
       </div>
-    </>
+
+      <div className={projectcardcss.cardslider}>
+        <Card
+          titleName="DPT"
+          paraInfo="DPT is a horror first person game..."
+          isDisabled={true}
+        />
+      </div>
+    </div>
   );
 }
+
 export default ProjectCards;
